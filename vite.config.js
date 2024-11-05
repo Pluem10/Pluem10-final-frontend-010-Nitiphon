00295a6cf251b850ddf141afbcc7b870f81aa7a2
Nitiphon Sombat <664259010@webmail.npru.ac.th>
2024-11-05 17:14:26 +0700

    config ud

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "final-frontend-010-Nitiphon",
+  base: "Pluem10-final-frontend-010-Nitiphon",
 });
